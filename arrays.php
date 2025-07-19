ec5a61c98c0f91a87143af0995203e502556101b
Abdullah123-Commits <abdullahmuaz002@gmail.com>
2025-07-19 23:19:28 +0500

    add variables.php and arrays.php

---
+++ arrays.php
@@ -0,0 +1,43 @@
+<?php
+    # WE WILL BE LOOKING AT ARRAYS AND LOOPS
+
+    /* THERE ARE 3 TYPES OF ARRAYS
+
+        1) indexed
+        2) associative
+        3) multi-dimensional arrays
+     */
+
+    /* 
+        LET'S LOOK AT INDEXED ARRAYS
+        There declaration and accessing is same as in C
+        e.g
+
+        print_r is a function that does the 
+        same working as echo does on variables
+
+        $cars = ["Honda","Toyota"];
+    print_r($cars);
+    $cars[] = "FORD";
+    print_r($cars);
+
+    count() is func that works on arrays and counts 
+    the no of elements present.
+
+    */
+
+    /* 
+        LET'S LOOK AT ASSOCIATIVE ARRAYS
+
+        
+        
+
+    */
+
+    
+    
+
+    
+
+    
+?>
---
+++ variables.php
@@ -0,0 +1,69 @@
+<?php
+    
+    # VAR DUMP FUNCTION
+    // USED TO FIND THE DATA TYPE OF ANY VARIABLE ALSO WORKS ON ARRAYS;
+    // var_dump(<var_name>);
+
+    /* ECHO VS PRINT FUNCTION
+
+        // echo() do not returns a value , generally used 
+        // echo "hello World!";
+
+        // print() does return a value but it is slower than echo() 
+        // print 'Hello World';
+    */
+
+    /*
+        VARIABLES IN PHP
+
+        without $ sign is a constant
+        can only start with letter or '_'
+
+        $output = 'Hello';
+        echo $output;
+     */
+
+    # also a single line comment
+
+    # DATA TYPES
+    /*
+        String either in single or double quotes;
+        Integer;
+        float;
+        Array;
+        Objects;
+        Resource => not a pure data type
+     */
+
+    /*  WORKING WITH VARAIBLES
+
+        // $num1 = 2;
+        // $num2 = 2;
+        // $sum = $num1 + $num2;
+        // $num3 = 4.5;
+        // echo $num3;
+
+        $str1 = 'hello';
+        $str2 = 'world';
+        # concatenation in JS is done via ' + ' operator
+        # but in PHP it is done via ' . ' operator
+
+        // echo $str1 .' '. $str2;
+        // echo "$str1 $str2";
+
+     */
+
+    /* DEFINING CONSTANTS
+
+        YOU DONT WANNA BE USING CONSTANTS WHEN YOU ARE UNSURE THAT THE VALUE WOULD BE CHANGED IN FUTURE !
+        define("GREETING","Hello Everyone!");
+        echo GREETING;
+     */
+    
+
+    
+    
+
+    
+
+?>
